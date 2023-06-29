be47309ffc3a36569a9f8faa81e9736ece25df30
Jpwrbd <Jpwrbd456@gmail.com>
2023-06-29 14:59:11 +0700

    lab

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,5 +1,16 @@
 //TODO: #3 provide me full syntax of C-Language contain include, main function(int).
-
+#include <stdio.h>
+  int main() {
+    printf("Hello world");
+    return 0;
+  }
 //TODO: #4 display me "Hello World." to me.
-
+#include <stdio.h>
+ 
+int main() {
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" );
+    printf( "This is my first time to coding with C language.\n" ) ;
+    printf( "Today is my birthday.\n" );
+    return 0 ;
+}
 //TODO: #5 Write your private info into Student-Info.txt
